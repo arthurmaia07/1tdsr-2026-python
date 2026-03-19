998961d2b3716d170ae04f113ae447159104b3a7
arthurmaia <arthurmaia2007@outlook.com>
2026-03-18 21:50:30 -0300

    commit aula 5

---
+++ aula-5/aula-5.py
@@ -0,0 +1,10 @@
+idade1 = int(input("sua idade: "))
+idade2 = int(input("idade do parceiro(a): "))
+
+result = idade1 / 2 + 7
+
+if idade2 >= result:
+    print("da pra pegar")
+
+else:
+    print("ta passando vergonha")
